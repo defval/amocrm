61a0e608637456b6a2ae0edd368cc4ef4a3bf48c
staygreengored <sggr90@gmail.com@>
2014-09-02 19:26:05 +0400

    changed author rename

--- amocrm/AmoCrm.php
+++ amocrm/AmoCrm.php
@@ -10,7 +10,7 @@ require_once 'methods/AmoLeadsList.php';
 /**
  * Your comment
  *
- * @author staygreengored <sggr90q@gmail.com>
+ * @author staygreengored <sggr90@gmail.com>
  * @since 1.0
  */
 class AmoCrm
