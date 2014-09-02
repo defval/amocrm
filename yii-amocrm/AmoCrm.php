141b2e82042387983a68965a56664d9ac2eced6a
mb24dev <mb24dev@gmail.com>
2014-09-02 19:22:38 +0400

    architecture and LeadsList method

---
+++ yii-amocrm/AmoCrm.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once 'base/AmoMethod.php';
+require_once 'base/AmoGetMethod.php';
+require_once 'base/AmoPostMethod.php';
+require_once 'interfaces/IMethod.php';
+require_once 'methods/AmoAuth.php';
+require_once 'methods/AmoLeadsList.php';
+
+/**
+ * Your comment
+ *
+ * @author staygreengored <sggr90q@gmail.com>
+ * @since 1.0
+ */
+class AmoCrm
+{
+
+    public $domain;
+
+    public $isAuth = false;
+
+    public function __construct($login = '', $hash = '', $domain = '')
+    {
+        $this->domain = $domain;
+
+        $auth = $this->auth(array(
+            'USER_LOGIN' => $login,
+            'USER_HASH'  => $hash
+        ));
+
+        if(!isset($auth['auth'])) {
+            throw new Exception('u dont auth');
+        };
+
+        $this->isAuth = true;
+
+    }
+
+    public function __call($name, $params) {
+        $className = 'Amo'.$name;
+        $params['domain'] = $this->domain;
+        $object = new $className($params);
+        return $object->run();
+    }
+}
---
+++ yii-amocrm/base/AmoPostMethod.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Your comment
+ *
+ * @author staygreengored <sggr90q@gmail.com>
+ * @since 1.0
+ */
+
+class AmoPostMethod extends AmoMethod {
+
+    /**
+     * @param $data
+     * @return mixed
+     */
+    public function post($data)
+    {
+
+        $curl = curl_init(); #Сохраняем дескриптор сеанса cURL
+
+        #Устанавливаем необходимые опции для сеанса cURL
+
+        curl_setopt($curl, CURLOPT_URL, $this->url);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+        $this->curlOptions($curl);
+
+
+        $out = curl_exec($curl); #Инициируем запрос к API и сохраняем ответ в переменную
+        $code = curl_getinfo($curl, CURLINFO_HTTP_CODE); #Получим HTTP-код ответа сервера
+        curl_close($curl); #Заверашем сеанс cURL
+
+        //Проверяем ответ CURL
+        $this->CheckCurlResponse($code);
+
+        /**
+         * Данные получаем в формате JSON, поэтому, для получения читаемых данных,
+         * нам придётся перевести ответ в формат, понятный PHP
+         */
+        $response = json_decode($out, true);
+
+        return $response['response'];
+    }
+
+    /**
+     * Устанавлтвает url для пост запросов
+     */
+    public function setUrl() {
+        $this->url = 'https://'.$this->domain.'.amocrm.ru'.$this->getMethodName();
+        $this->url .= '?type='.$this->type;
+    }
+} 
