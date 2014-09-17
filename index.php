2354f3832f2b6786b876c2671613d90680a7dea6
mb24dev <mb24dev@gmail.com>
2014-09-17 17:30:06 +0400

    if-modified-since modif

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 require_once 'AmoCrm.php';
 try {
     $result = new AmoCrm('pivnev.wlad@gmail.com', '837d208087635577386521ba04681ae7', 'visaglobus');
-    $r = $result->NotesList(array('type' => 'lead'));
+    $r = $result->ContactsList(array('id' => array(23020462, 23020350)));
     echo '<pre>';
     print_r($r);
     echo '</pre>';
